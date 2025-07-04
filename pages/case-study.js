be89a5833a2db3cfef38155aaba88f354fa3f13d
kevinchdra <kevinchdra21@gmail.com>
2025-07-05 00:26:51 +0700

    Update case-study.js

--- pages/case-study.js
+++ pages/case-study.js
@@ -1,8 +1,9 @@
 document.addEventListener("DOMContentLoaded", () => {
   const images = document.querySelectorAll(".images img");
+  const videos = document.querySelectorAll(".image-grid video");
 
   images.forEach((img, index) => {
-    const delay = index * 100; // 200ms delay between each image
+    const delay = index * 100;
 
     if (img.complete) {
       setTimeout(() => {
@@ -16,4 +17,48 @@ document.addEventListener("DOMContentLoaded", () => {
       });
     }
   });
+
+  videos.forEach((video, index) => {
+    const delay = index * 100;
+
+    if (video.readyState >= 2) {
+      // Enough data is loaded to render the video
+      setTimeout(() => {
+        video.classList.add("loaded");
+      }, delay);
+    } else {
+      video.addEventListener("loadeddata", () => {
+        setTimeout(() => {
+          video.classList.add("loaded");
+        }, delay);
+      });
+    }
+  });
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const videos = document.querySelectorAll(".images video");
+
+  videos.forEach((video, index) => {
+    const delay = index * 100;
+
+    if (video.readyState >= 2) {
+      setTimeout(() => {
+        video.classList.add("loaded");
+      }, delay);
+    } else {
+      video.addEventListener("loadeddata", () => {
+        setTimeout(() => {
+          video.classList.add("loaded");
+        }, delay);
+      });
+    }
+  });
+});
+
+
+  const video = document.getElementById("myVideo");
+
+  video.addEventListener("loadedmetadata", () => {
+    video.currentTime = 3; // starts at 5 seconds
+  });
